014fe2d5a5c68e3babaed6eefcef6402ab4b9fa6
leonelmaken <103751176+leonelmaken@users.noreply.github.com>
2025-02-18 18:25:30 +0100

    tontine init

---
+++ Tontine/src/main/java/com/Tontine/Tontine/TontineApplication.java
@@ -0,0 +1,13 @@
+package com.Tontine.Tontine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TontineApplication.class, args);
+	}
+
+}
---
+++ Tontine/src/test/java/com/Tontine/Tontine/TontineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tontine.Tontine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
